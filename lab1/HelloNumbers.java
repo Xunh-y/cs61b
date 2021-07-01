afbc115c4573b6a2e060cbfbd7fd57225c3eebf5
xunh <471046960@qq.com>
2021-07-01 20:31:16 +0800

    lab1 first

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,10 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int x = 0;
+        while (x < 10) {
+            System.out.print(x + " ");
+            x = x + 1;
+        }
+        System.out.println(x);
+    }
+}
--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -3,6 +3,15 @@
  */
 public class LeapYear {
 
+    public static boolean isLeapYear(int year){
+        if(year%400==0){
+	return true;
+        }
+        else if(year%4==0&&year%100!=0){
+	return true;
+        }
+        else return false;
+    }
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
