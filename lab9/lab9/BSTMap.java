461d1c04326c9edd8dd40631b62c79ca4fb6bdbb
xunh <471046960@qq.com>
2021-07-17 14:18:21 +0800

    lab9

--- lab9/lab9/BSTMap.java
+++ lab9/lab9/BSTMap.java
@@ -1,5 +1,6 @@
 package lab9;
 
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -18,16 +19,15 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
         /* Children of this Node. */
         private Node left;
         private Node right;
-
         private Node(K k, V v) {
             key = k;
             value = v;
         }
     }
-
     private Node root;  /* Root node of the tree. */
     private int size; /* The number of key-value pairs in the tree */
-
+    private boolean add;
+    private V removeValue;
     /* Creates an empty BSTMap. */
     public BSTMap() {
         this.clear();
@@ -38,42 +38,53 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
     public void clear() {
         root = null;
         size = 0;
+        add = true;
     }
 
-    /** Returns the value mapped to by KEY in the subtree rooted in P.
-     *  or null if this map contains no mapping for the key.
-     */
-    private V getHelper(K key, Node p) {
-        throw new UnsupportedOperationException();
+    private V getHelper(K key, Node root) {
+        if (root == null) {
+            return null;
+        }
+        if (root.key.equals(key)) {
+            return root.value;
+        } else if (key.compareTo(root.key) < 0) {
+            return getHelper(key, root.left);
+        } else return getHelper(key, root.right);
     }
 
-    /** Returns the value to which the specified key is mapped, or null if this
-     *  map contains no mapping for the key.
-     */
     @Override
     public V get(K key) {
-        throw new UnsupportedOperationException();
+        return getHelper(key, root);
     }
 
-    /** Returns a BSTMap rooted in p with (KEY, VALUE) added as a key-value mapping.
-      * Or if p is null, it returns a one node BSTMap containing (KEY, VALUE).
-     */
-    private Node putHelper(K key, V value, Node p) {
-        throw new UnsupportedOperationException();
+    private Node putHelper(K key, V value, Node root) {
+        if (root == null) {
+            add = true;
+            return new Node(key, value);
+        }
+        if (key.compareTo(root.key) < 0) {
+            root.left = putHelper(key, value, root.left);
+        } else if (key.compareTo(root.key) > 0) {
+            root.right = putHelper(key, value, root.right);
+        } else {
+            root = new Node(key, value);
+            add = false;
+        }
+        return root;
     }
 
-    /** Inserts the key KEY
-     *  If it is already present, updates value to be VALUE.
-     */
     @Override
     public void put(K key, V value) {
-        throw new UnsupportedOperationException();
+        root = putHelper(key, value, root);
+        if (add) {
+            size++;
+        }
     }
 
     /* Returns the number of key-value mappings in this map. */
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return size;
     }
 
     //////////////// EVERYTHING BELOW THIS LINE IS OPTIONAL ////////////////
@@ -81,7 +92,18 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
     /* Returns a Set view of the keys contained in this map. */
     @Override
     public Set<K> keySet() {
-        throw new UnsupportedOperationException();
+        Set<K> set = new HashSet<>();
+        keySetHelper(root, set);
+        return set;
+    }
+
+    private void keySetHelper(Node root, Set<K> set) {
+        if (root == null) {
+            return;
+        }
+        set.add(root.key);
+        keySetHelper(root.left, set);
+        keySetHelper(root.right, set);
     }
 
     /** Removes KEY from the tree if present
@@ -90,7 +112,48 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
      */
     @Override
     public V remove(K key) {
-        throw new UnsupportedOperationException();
+        root = removeHelper(key, root);
+        return removeValue;
+    }
+
+    private Node removeHelper(K key, Node root) {
+        if (root == null) {
+            return null;
+        }
+        if (key.compareTo(root.key) < 0) {
+            root.left = removeHelper(key, root.left);
+        } else if (key.compareTo(root.key) > 0) {
+            root.right = removeHelper(key, root.right);
+        } else {
+            if (root.left == null) {
+                return root.right;
+            }
+            if (root.right == null) {
+                return root.left;
+            }
+            removeValue = root.value;
+            Node newNode = min(root.right);
+            newNode.left = root.left;
+            newNode.right = rotate(root.right);
+            return newNode;
+        }
+        return root;
+    }
+
+    private Node rotate(Node root) {
+        if (root.left == null) {
+            return root.right;
+        }
+        root.left = rotate(root.left);
+        size--;
+        return root;
+    }
+
+    private Node min(Node root) {
+        if (root.left == null) {
+            return root;
+        }
+        return min(root.left);
     }
 
     /** Removes the key-value entry for the specified key only if it is
@@ -99,11 +162,31 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
      **/
     @Override
     public V remove(K key, V value) {
-        throw new UnsupportedOperationException();
+        if (get(key) != value) {
+            return null;
+        }
+        return remove(key);
     }
 
     @Override
     public Iterator<K> iterator() {
-        throw new UnsupportedOperationException();
+        return new BSTIterator();
+    }
+
+    private class BSTIterator implements Iterator<K>{
+        private Iterator<K> cur;
+
+        public BSTIterator() {
+            cur = keySet().iterator();
+        }
+        @Override
+        public boolean hasNext() {
+            return cur.hasNext();
+        }
+
+        @Override
+        public K next() {
+            return cur.next();
+        }
     }
 }
