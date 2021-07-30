6b13b2837bd13fa225e60df3328cd5bffd12f02f
xunh <471046960@qq.com>
2021-07-30 15:09:01 +0800

    lab13 lsd

--- lab13/RadixSort.java
+++ lab13/RadixSort.java
@@ -5,6 +5,25 @@
  *
  */
 public class RadixSort {
+
+    public static void main(String[] args) {
+        String[] s1 = {"dwa", "1ad2", "sad2", "8s8", "2222", "2221", "uu2"};
+        for (String s : s1) {
+            System.out.println(s);
+        }
+        s1 = sort(s1);
+        for (String s : s1) {
+            System.out.println(s);
+        }
+        String[] s2 = {"11", "1004", "2199", "22", "208888", "234", "2341"};
+        for (String s : s2) {
+            System.out.println(s);
+        }
+        s2 = sort(s2);
+        for (String s : s2) {
+            System.out.println(s);
+        }
+    }
     /**
      * Does LSD radix sort on the passed in array with the following restrictions:
      * The array can only have ASCII Strings (sequence of 1 byte characters)
@@ -16,8 +35,16 @@ public class RadixSort {
      * @return String[] the sorted array
      */
     public static String[] sort(String[] asciis) {
-        // TODO: Implement LSD Sort
-        return null;
+        int maxPos = 0;
+        for (String s : asciis) {
+            if (s.length() > maxPos) {
+                maxPos = s.length();
+            }
+        }
+        for (int d = 0; d < maxPos; d++) {
+            sortHelperLSD(asciis, d);
+        }
+        return asciis;
     }
 
     /**
@@ -27,8 +54,40 @@ public class RadixSort {
      * @param index The position to sort the Strings on.
      */
     private static void sortHelperLSD(String[] asciis, int index) {
-        // Optional LSD helper method for required LSD radix sort
+        int r = 256;
+        int[] cnt = new int[r + 1];
+        for (String item: asciis) {
+            int c = getchar(item, index);
+            cnt[c]++;
+        }
+
+        int[] starts = new int[r + 1];
+        int pos = 0;
+        for (int i = 0; i < starts.length; i += 1) {
+            starts[i] = pos;
+            pos += cnt[i];
+        }
+
+        String[] sorted = new String[asciis.length];
+        for (int i = 0; i < asciis.length; i += 1) {
+            String a = asciis[i];
+            int item = getchar(a, index);
+            int place = starts[item];
+            sorted[place] = a;
+            starts[item] += 1;
+        }
+        for (int i = 0; i < sorted.length; ++i) {
+            asciis[i] = sorted[i];
+        }
         return;
+    }
+
+    private static int getchar(String item, int index) {
+        if (item.length() > index) {
+            return item.charAt(index) + 1;
+        } else {
+            return 0;
+        }
     }
 
     /**
